efd139c1004cc2bc5af09e4035843d0351a0a417
Marnen Laibow-Koser <marnen@marnen.org>
2009-11-04 00:09:55 -0500

    Put a table in the document window. [#1]

--- src/document/Document.java
+++ src/document/Document.java
@@ -31,29 +31,31 @@ public class Document extends javax.swing.JFrame {
   // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
   private void initComponents() {
 
-    jLabel1 = new javax.swing.JLabel();
+    jScrollPane1 = new javax.swing.JScrollPane();
+    jTable1 = new javax.swing.JTable();
 
     setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
     setTitle("Document");
 
-    jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-    jLabel1.setText("jLabel1");
+    jTable1.setModel(new javax.swing.table.DefaultTableModel(
+      new Object [][] {
+        {"a", "b", "c", "d"}
+      },
+      new String [] {
+        "Title1", "Title2", "Title3", "Title4"
+      }
+    ));
+    jScrollPane1.setViewportView(jTable1);
 
     javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
     getContentPane().setLayout(layout);
     layout.setHorizontalGroup(
       layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-      .addGroup(layout.createSequentialGroup()
-        .addGap(175, 175, 175)
-        .addComponent(jLabel1)
-        .addContainerGap(180, Short.MAX_VALUE))
+      .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 632, Short.MAX_VALUE)
     );
     layout.setVerticalGroup(
       layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-      .addGroup(layout.createSequentialGroup()
-        .addGap(47, 47, 47)
-        .addComponent(jLabel1)
-        .addContainerGap(237, Short.MAX_VALUE))
+      .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 387, Short.MAX_VALUE)
     );
 
     pack();
@@ -71,7 +73,8 @@ public class Document extends javax.swing.JFrame {
     }
 
   // Variables declaration - do not modify//GEN-BEGIN:variables
-  private javax.swing.JLabel jLabel1;
+  private javax.swing.JScrollPane jScrollPane1;
+  private javax.swing.JTable jTable1;
   // End of variables declaration//GEN-END:variables
 
 }
