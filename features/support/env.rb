283c22a832c650112c9077f3e7bdb68077fdbd8d
Marnen Laibow-Koser <marnen@marnen.org>
2009-11-12 11:33:50 -0500

    Move features and Swinger outside of the spec directory so autospec will pick them up. [#2]

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,12 +1,12 @@
 path = File.expand_path(File.dirname(__FILE__))
-require File.expand_path(path + '/../../lib/swinger/lib/swinger')
+require File.expand_path(path + '/../lib/swinger/lib/swinger')
 
-$CLASSPATH << path + '/../../../package/classes'
+$CLASSPATH << path + '/../../package/classes'
 
 puts "It takes a while for the scenarios to begin executing, so please be patient..."
 
 $NO_MAC_MENUBAR = true # disable Mac-style menu bars so Swinger can drive the menus
-require path + '/../../../src/main'
+require path + '/../../src/main'
 sleep(1)
 
 at_exit do 
