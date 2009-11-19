354d9a0bf69caee2bb41b559b77da9abc0538ca4
Marnen Laibow-Koser <marnen@marnen.org>
2009-11-19 16:13:25 -0500

    Move class methods into one place.

--- src/application_controller.rb
+++ src/application_controller.rb
@@ -4,41 +4,43 @@ class ApplicationController < Monkeybars::Controller
 
   load_gettext
 
-  # Closes all windows and otherwise resets the application to a blank slate.
-  def self.reset
-    DocumentController.active_controllers.values do |c|
-      c.close
+  class << self
+    # Closes all windows and otherwise resets the application to a blank slate.
+    def reset
+      DocumentController.active_controllers.values do |c|
+        c.close
+      end
+      DocumentController.send :reset_count
     end
-    DocumentController.send :reset_count
-  end
 
-  # Displays a file chooser dialog, and returns the full pathname of the selected file, or nil if the dialog was cancelled.
-  # TODO: Should this be in its own class?  Should it return the File object itself?
-  def self.choose_file
-    if $MOCK_FILE_CHOOSERS # Fake the dialog since Swinger can't drive AWT widgets
-      choose_file_mock
-    else
-      choose_file_awt
+    # Displays a file chooser dialog, and returns the full pathname of the selected file, or nil if the dialog was cancelled.
+    # TODO: Should this be in its own class?  Should it return the File object itself?
+    def choose_file
+      if $MOCK_FILE_CHOOSERS # Fake the dialog since Swinger can't drive AWT widgets
+        choose_file_mock
+      else
+        choose_file_awt
+      end
     end
-  end
 
-  protected
+    protected
 
-  def self.choose_file_awt
-    dialog = java.awt.FileDialog.new(nil, _('New'), java.awt.FileDialog::SAVE)
-    dialog.show
-    if dialog.file.nil? # User cancelled without selecting a file
-      return nil
+    def choose_file_awt
+      dialog = java.awt.FileDialog.new(nil, _('New'), java.awt.FileDialog::SAVE)
+      dialog.show
+      if dialog.file.nil? # User cancelled without selecting a file
+        return nil
+      end
+      return dialog.directory + dialog.file
     end
-    return dialog.directory + dialog.file
-  end
 
-  def self.choose_file_mock
-    filename = javax.swing.JOptionPane.show_input_dialog(nil, _('Enter file path:'), _('New'), javax.swing.JOptionPane::QUESTION_MESSAGE)
-    if filename.nil? # User cancelled
-      return nil
-    else
-      return File.expand_path filename
+    def choose_file_mock
+      filename = javax.swing.JOptionPane.show_input_dialog(nil, _('Enter file path:'), _('New'), javax.swing.JOptionPane::QUESTION_MESSAGE)
+      if filename.nil? # User cancelled
+        return nil
+      else
+        return File.expand_path filename
+      end
     end
   end
 end
