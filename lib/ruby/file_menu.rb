96504a9cb271ee398f7a759d395a8763831f1796
Marnen Laibow-Koser <marnen@marnen.org>
2009-11-29 23:45:48 -0500

    Remove more duplication. [#4]

--- lib/ruby/file_menu.rb
+++ lib/ruby/file_menu.rb
@@ -1,12 +1,25 @@
 # Mixin to implement standard File menu actions.
 module FileMenu
   def file_new_menu_item_action_performed
-    instance = DocumentController.create_instance
-    instance.open unless instance.nil?
+    new_document
   end
 
   def file_open_menu_item_action_performed
-    instance = DocumentController.create_instance(nil, false)
+    open_document
+  end
+
+  private
+
+  def new_document
+    get_document
+  end
+
+  def open_document
+    get_document(nil, false)
+  end
+
+  def get_document(*args)
+    instance = DocumentController.create_instance(*args)
     instance.open unless instance.nil?
   end
 end
